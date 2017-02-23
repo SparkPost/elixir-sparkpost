08720cf49de8aa86c5e4042f2f13c2411b726495
Ashu Goel <ashutosh.goel@teamaol.com>
2017-02-22 16:51:21 -0800

    [template_test] remove describe block since travis runs 1.2

--- test/template_test.exs
+++ test/template_test.exs
@@ -24,60 +24,58 @@ defmodule SparkPost.TemplateTest do
     end
   end
 
-  describe "preview tests" do
-    test_with_mock "Template.preview succeeds with Template.ContentResponse",
-      HTTPoison, [request: fn (method, url, body, headers, opts) ->
-        assert method == :post
-        # draft not set
-        assert String.ends_with?(url, "preview")
-        fun = MockServer.mk_http_resp(200, MockServer.get_json("previewtemplate"))
-        fun.(method, url, body, headers, opts)
-      end] do
-        resp = Template.preview(TestStruct.basic_template(), TestStruct.substitution_data())
-        assert %ContentResponse{} = resp
-    end
+  test_with_mock "Template.preview succeeds with Template.ContentResponse",
+    HTTPoison, [request: fn (method, url, body, headers, opts) ->
+      assert method == :post
+      # draft not set
+      assert String.ends_with?(url, "preview")
+      fun = MockServer.mk_http_resp(200, MockServer.get_json("previewtemplate"))
+      fun.(method, url, body, headers, opts)
+    end] do
+      resp = Template.preview(TestStruct.basic_template(), TestStruct.substitution_data())
+      assert %ContentResponse{} = resp
+  end
 
-    test_with_mock "Template.preview succeeds with Template.ContentResponse and draft set",
-      HTTPoison, [request: fn (method, url, body, headers, opts) ->
-        assert method == :post
-        assert String.ends_with?(url, "preview?draft=true")
-        fun = MockServer.mk_http_resp(200, MockServer.get_json("previewtemplate"))
-        fun.(method, url, body, headers, opts)
-      end] do
-        resp = Template.preview(TestStruct.template_with_draft(), TestStruct.substitution_data())
-        assert %ContentResponse{} = resp
-    end
+  test_with_mock "Template.preview succeeds with Template.ContentResponse and draft set",
+    HTTPoison, [request: fn (method, url, body, headers, opts) ->
+      assert method == :post
+      assert String.ends_with?(url, "preview?draft=true")
+      fun = MockServer.mk_http_resp(200, MockServer.get_json("previewtemplate"))
+      fun.(method, url, body, headers, opts)
+    end] do
+      resp = Template.preview(TestStruct.template_with_draft(), TestStruct.substitution_data())
+      assert %ContentResponse{} = resp
+  end
 
-    test_with_mock "Template.preview fails with Endpoint.Error", HTTPoison,
-      [request: MockServer.mk_fail] do
-        resp = Template.preview(TestStruct.basic_template(), TestStruct.substitution_data())
-        assert %Endpoint.Error{} = resp
-    end
+  test_with_mock "Template.preview fails with Endpoint.Error", HTTPoison,
+    [request: MockServer.mk_fail] do
+      resp = Template.preview(TestStruct.basic_template(), TestStruct.substitution_data())
+      assert %Endpoint.Error{} = resp
+  end
 
-    test_with_mock "Template.preview unmarshals complex from field correctly", HTTPoison,
-      [request: fn (method, url, body, headers, opts) ->
-        assert method == :post
-        fun = MockServer.mk_http_resp(200, MockServer.get_json("previewtemplate"))
-        fun.(method, url, body, headers, opts)
-      end] do
-        resp = Template.preview(TestStruct.basic_template(), TestStruct.substitution_data())
-        assert %SparkPost.Address{
-          name: "Example Company Marketing",
-          "email": "marketing@bounces.company.example"
-        } == resp.from
-    end
+  test_with_mock "Template.preview unmarshals complex from field correctly", HTTPoison,
+    [request: fn (method, url, body, headers, opts) ->
+      assert method == :post
+      fun = MockServer.mk_http_resp(200, MockServer.get_json("previewtemplate"))
+      fun.(method, url, body, headers, opts)
+    end] do
+      resp = Template.preview(TestStruct.basic_template(), TestStruct.substitution_data())
+      assert %SparkPost.Address{
+        name: "Example Company Marketing",
+        "email": "marketing@bounces.company.example"
+      } == resp.from
+  end
 
-    test_with_mock "Template.preview unmarshals simple from field correctly", HTTPoison,
-      [request: fn (method, url, body, headers, opts) ->
-        assert method == :post
-        fun = MockServer.mk_http_resp(200, MockServer.get_json("previewtemplate_simpleemail"))
-        fun.(method, url, body, headers, opts)
-      end] do
-        resp = Template.preview(TestStruct.basic_template(), TestStruct.substitution_data())
-        assert %SparkPost.Address{
-          name: nil,
-          "email": "marketing@bounces.company.example"
-        } == resp.from
-    end
+  test_with_mock "Template.preview unmarshals simple from field correctly", HTTPoison,
+    [request: fn (method, url, body, headers, opts) ->
+      assert method == :post
+      fun = MockServer.mk_http_resp(200, MockServer.get_json("previewtemplate_simpleemail"))
+      fun.(method, url, body, headers, opts)
+    end] do
+      resp = Template.preview(TestStruct.basic_template(), TestStruct.substitution_data())
+      assert %SparkPost.Address{
+        name: nil,
+        "email": "marketing@bounces.company.example"
+      } == resp.from
   end
 end
