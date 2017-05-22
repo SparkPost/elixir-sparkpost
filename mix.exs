df668f6f5529b4e1671209ac1cad0b421ff32072
Ewan Dennis <ewan.dennis@sparkpost.com>
2017-05-22 16:29:03 +0100

    0.5.1 bump, removed ibrowse from README

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule SparkPost.Mixfile do
 
   def project do
     [app: :sparkpost,
-     version: "0.5.0",
+     version: "0.5.1",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
