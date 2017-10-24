451b2786b507b71ef8e616e33907e1591f0d6ec1
samuelbambu <samuel@bambucode.com>
2017-10-24 17:16:26 -0600

    attachments example, back to original

--- examples/attachment.exs
+++ examples/attachment.exs
@@ -1,13 +1,19 @@
-from = "soporte@eleventa.com"
-to = "dev.sam23d@gmail.com"
+from = "elixir@sparkpostbox.com"
+to = "ewan.dennis@sparkpost.com"
+filename = "test/data/sparky.png"
 
 SparkPost.Transmission.send(
   %SparkPost.Transmission{
-    recipients: [ %SparkPost.Recipient{ address: %SparkPost.Address{ email: to } } ],
+    recipients: [ to ],
     content: %SparkPost.Content.Inline{
       from: from,
       subject: "Now with attachments!",
-      text: "There is an attachment with this message"
+      text: "There is an attachment with this message",
+      attachments: [
+        SparkPost.Content.to_attachment(
+          Path.basename(filename), "image/jpeg", File.read!(filename)
+        )
+      ]
     }
   }
 )
